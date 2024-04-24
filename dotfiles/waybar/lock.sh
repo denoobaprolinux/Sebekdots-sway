66d4afd24e8b9754e8fcaf16d76d2bbb3e369080
Andrés Díaz <121652305+andrewsebek@users.noreply.github.com>
2024-04-24 02:46:40 -0400

    Configuraciones de Waybar y Swaync

---
+++ dotfiles/waybar/lock.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+swaylock -i /$HOME/.cache/current_wallpaper.jpg --clock --indicator-idle-visible \
+        --indicator-radius 100 \
+        --indicator-thickness 7 \
+        --ring-color 455a64 \
+        --key-hl-color be5046 \
+        --text-color ffc107 \
+        --line-color 00000000 \
+        --inside-color 00000088 \
+        --separator-color 00000000 \
+        --fade-in 0.1 \
+        --effect-scale 0.5 --effect-blur 7x3 --effect-scale 2 \
+        --effect-vignette 0.5:0.5 \
