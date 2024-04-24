809a5bf469ba4e84a83ec2870eb0b265065d1420
Andrés Díaz <121652305+andrewsebek@users.noreply.github.com>
2024-04-24 02:45:14 -0400

    Delete dotfiles/waybar directory

--- dotfiles/waybar/menubottom.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-nwg-menu -va bottom -ml 10 -cmd-lock "/$HOME/dotfiles/waybar/lock.sh" -cmd-logout "swaymsg exit" -cmd-restart "reboot" -cmd-shutdown "poweroff" -fm "nautilus" -d -isl 20 -iss 10
