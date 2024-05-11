93522412610be9e98b0298e136e35933c35fb33b
Andrés Díaz <121652305+andrewsebek@users.noreply.github.com>
2024-05-11 04:42:42 -0400

    Actualizado menutop.sh

--- dotfiles/waybar/menutop.sh
+++ dotfiles/waybar/menutop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nwg-menu -va top -ml 10 -cmd-lock "swaylock -i /$HOME/.cache/current_wallpaper.jpg" -cmd-logout "swaymsg exit" -cmd-restart "reboot" -cmd-shutdown "poweroff" -fm "nautilus" -d -isl 20 -iss 10
+nwg-menu -va top -ml 10 -cmd-lock "hyprlock" -cmd-logout "swaymsg exit" -cmd-restart "reboot" -cmd-shutdown "poweroff" -fm "nautilus" -d -isl 20 -iss 10
