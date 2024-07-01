6bf8e8200d12800950c4befdb8557f9bdd8cfb5d
Andrés Díaz <andrew.keyboards@gmail.com>
2024-07-01 15:09:06 -0400

    Cambios de Sebekdots Sway 3

--- dotfiles/waybar/wallpaper.sh
+++ dotfiles/waybar/wallpaper.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Crear la carpeta ./cache/wallpapers si no existe
+thumbs_dir="$HOME/.cache/wallpapers"
+mkdir -p "$thumbs_dir"
+
+# Función para generar miniaturas
+generate_thumbnail() {
+    local wallpaper="$1"
+    local thumbnail="$thumbs_dir/$(basename "$wallpaper").png"
+    if [ ! -f "$thumbnail" ]; then
+        magick convert "$wallpaper" -thumbnail 200x200 "$thumbnail"
+    fi
+}
 
 ## Crear archivos caché con la información del wallpaper
 cache_file="$HOME/.cache/current_wallpaper"
@@ -35,7 +47,8 @@ case $1 in
 
         selected=$( find "$HOME/Imágenes/Wallpapers/Set/" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -exec basename {} \; | sort -R | while read rfile
         do
-            echo -en "$rfile\x00icon\x1f$HOME/Imágenes/Wallpapers/Set/${rfile}\n"
+            generate_thumbnail "$HOME/Imágenes/Wallpapers/Set/$rfile"  # Generar miniatura
+            echo -en "$rfile\x00icon\x1f$thumbs_dir/${rfile}.png\n"
         done | rofi -dmenu -replace -disable-history -sort -config ~/.config/rofi/config-wallpaper.rasi)
         if [ ! "$selected" ]; then
             echo "No wallpaper selected"
@@ -64,7 +77,7 @@ echo "Wallpaper: $wallpaper"
 echo "$wallpaper" > "$cache_file"
 echo "* { current-image: url(\"$wallpaper\", height); }" > "$rasi_file"
 cp $wallpaper ~/.cache/current_wallpaper.jpg
-convert ~/.cache/current_wallpaper.jpg ~/.cache/current_wallpaper.png # instalar imagemagick para que esto funcione
+magick ~/.cache/current_wallpaper.jpg ~/.cache/current_wallpaper.png # instalar imagemagick para que esto funcione
 
 # ----------------------------------------------------- 
 # Matar Swaync y Waybar, y volver a cargar todo
@@ -83,11 +96,6 @@ swww img ~/.cache/current_wallpaper.jpg \
      --transition-fps=60 \
      --transition-type="simple" \
      --transition-duration=0.5 \
-#     --transition-pos "$( hyprctl cursorpos )"
 
-# ----------------------------------------------------- 
-# Envía la notificación
-# ----------------------------------------------------- 
-notify-send "Colores y Fondo de Pantalla" "con imagen $newwall"
 
-echo "Listo!"
+
